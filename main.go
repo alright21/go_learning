37d8daf3b4838730c7570e9c95bb5f85e9fa5027
alright21 <alberto.giust@studenti.unitn.it>
2021-05-19 17:49:10 +0300

    feat(main): variables

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "fmt" 
+import (
+	"fmt"
+	"reflect"
+)
+
 
 func main(){
 	// simple hello world in GO
 	fmt.Println("Hello World")
+
+	// variables
+	var name = "Hello"
+	fmt.Println(reflect.TypeOf(name))
+
+	var number = 10
+	fmt.Println(reflect.TypeOf(number))
+
+	// initial definitio operator
+	x:= 11
+	fmt.Println(x)
+
+	//constants
+	const PI = 3.14 
+	fmt.Println(PI)
+
+	//booleans
+	t:=true
+	fmt.Println(t)
+	
 }
