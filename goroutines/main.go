dd62e65a272b472f86b33f15ab1bbc97e2160e8e
alright21 <alberto.giust@studenti.unitn.it>
2021-05-21 11:58:17 +0300

    feat(goroutines): add async and ordinary channel example

---
+++ goroutines/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func printHello(){
+	fmt.Println("Hello World")
+	// time.Sleep(2 * time.Second)
+	fmt.Println("Done waiting")
+}
+
+func printBye(){
+	fmt.Println("Bye World")
+}
+// send into the channel
+func sendInfo(channel chan<- string, info string){
+	channel <- info
+}
+
+// extract from the channel
+func printInfo(channel <-chan string){
+	fmt.Println(<-channel)
+}
+
+func main(){
+
+	// async function example
+	go printHello()
+	go printBye()
+
+	information := make(chan string)
+	// ordinary channels example
+	go printInfo(information)
+	go sendInfo(information,"Hello")
+
+
+	time.Sleep(2 * time.Second)
+
+
+
+}
